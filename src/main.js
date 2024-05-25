df5ca3343cc3dce3eabb2724e5a3391ecfeb71a5
Vlad Kishkovar <kishkovarv@gmail.com>
2024-05-25 09:50:32 +0300

    Fix bug js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ function Welcome() {
     console.log("Welocome To Git naxyi")
 
     function hello() {
-        console.log("Hello man")
+        console.log("Hello mans")
     }
 
     console.log("Hello man")
