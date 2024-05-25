bb98dfc2947bc2c453e255ee7f4a23d427f9f353
Vlad Kishkovar <kishkovarv@gmail.com>
2024-05-25 10:38:46 +0300

    New fucntion

--- src/one.js
+++ src/one.js
@@ -1,14 +1,14 @@
 // Функция №1
-function complexFunction1() {
-    let result = 0;
-    for (let i = 0; i < 1000; i++) {
-        result += Math.sin(i) * Math.cos(i) + Math.sqrt(i) + Math.log(i + 1);
+function complexFunction2() {
+    let result = 1;
+    for (let i = 1; i <= 100; i++) {
+        result *= i;
     }
-    for (let j = 0; j < 500; j++) {
-        result -= Math.pow(j, 2) / Math.sqrt(j + 1);
+    for (let j = 1; j <= 50; j++) {
+        result += Math.pow(j, 3) - Math.sqrt(j) + Math.log(j + 1);
     }
-    for (let k = 0; k < 200; k++) {
-        result *= Math.random() * (k + 1);
+    for (let k = 1; k <= 200; k++) {
+        result /= Math.random() * (k + 1);
     }
     return result;
 }
